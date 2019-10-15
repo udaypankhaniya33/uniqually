359abf82c2062536c78582fdedab3a74751cf553
Buddhika Herath <buddhikaherath.dev@gmail.com>
2019-10-15 14:58:43 +0530

    Global constances config initiated with user types

---
+++ config/constances.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This configuration file holds global constance variables for the centralized API
+ * ---------------------------------------------------------------------------------------------------------------------
+ */
+
+return [
+
+    /*
+     * Allowed user types in vManageTax platform
+     * Note :: This array structure may change with future developments
+     * -----------------------------------------------------------------------------------------------------------------
+     * */
+    'user_types' => [
+        'ADMIN'     => 1,
+        'CUSTOMER'  => 2,
+        'OM'        => 3,
+        'EXPERT'    => 4
+    ]
+
+];
