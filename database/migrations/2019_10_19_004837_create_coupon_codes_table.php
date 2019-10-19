c4da272715fbd32215493efaf7c7dc6b6efc82d6
Buddhika Herath <buddhikaherath.dev@gmail.com>
2019-10-19 06:22:44 +0530

    Coupon codes model development

---
+++ database/migrations/2019_10_19_004837_create_coupon_codes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupon_codes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->decimal('discount');
+            $table->boolean('is_enabled')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupon_codes');
+    }
+}
