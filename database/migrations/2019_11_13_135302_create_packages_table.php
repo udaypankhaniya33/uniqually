832a9ac7387074cb89aab1bf655f30e582624061
Buddhika Herath <buddhikaherath.dev@gmail.com>
2019-11-13 20:12:37 +0530

    Package model development, and migrations

---
+++ database/migrations/2019_11_13_135302_create_packages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->decimal('price')->default(0.00);
+            $table->unsignedBigInteger('package_category_id');
+            $table->timestamps();
+            $table->foreign('package_category_id')->references('id')->on('package_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
