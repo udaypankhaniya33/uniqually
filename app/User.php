f487f3b6711e63c12dcdb5cf0a43ce6ca42c8db1
Buddhika Herath <buddhikaherath.dev@gmail.com>
2019-11-15 08:32:50 +0530

    User model updated with first name & last name columns

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'type'
+        'name', 'email', 'password', 'type', 'first_name', 'last_name'
     ];
 
     /**
