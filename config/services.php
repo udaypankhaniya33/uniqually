3437ab52a97aefda526dc75eee13ab0b28909206
Buddhika Herath <buddhikaherath.dev@gmail.com>
2019-12-21 22:19:40 +0000

    Revert "Social media authentication test version"
    This reverts commit f75fedca402583bfc4bbdf13e870fa4f69e82d21

--- config/services.php
+++ config/services.php
@@ -30,10 +30,4 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
-    'facebook' => [
-        'client_id' => env('FACEBOOK_CLIENT_ID'),
-        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' =>  env('FACEBOOK_CALLBACK_URL'),
-    ],
-
 ];
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('welcome')->name('main_page');
+    return view('welcome');
 });
