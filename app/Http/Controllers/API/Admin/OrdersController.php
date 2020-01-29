e51f48da00c504e380ecc146b708b92a98cdac28
Buddhika Herath <buddhikaherath.dev@gmail.com>
2020-01-29 11:25:28 +0530

    Get all order details API response updated

--- app/Http/Controllers/API/Admin/OrdersController.php
+++ app/Http/Controllers/API/Admin/OrdersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\API\Admin;
 
 use App\Http\Controllers\API\BaseController;
 use App\Order;
+use App\Package;
+use App\PackageAddon;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -17,13 +19,23 @@ class OrdersController extends BaseController
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        $orders = Order::with('orderAddons')->get();
+        $orders = Order::with('orderAddons')->orderBy('created_at', 'desc')->get();
         foreach ($orders as $index => $order){
             $creator = User::where('id', $order->order_created_by)->first();
+            $package = Package::where('id', $order->base_package_id)->with('packageCategory')->first();
+            $order->basePaackage = [
+                'name' => $package->packageCategory->title.' - '.$package->title,
+                'price' => $package->discounted_price
+            ];
             $order->user = [
                 'name' => decrypt($creator->name),
                 'email' => $creator->email
             ];
+            foreach ($order->orderAddons as $addon){
+                $addonData = PackageAddon::where('id', $addon->package_addon_id)->first();
+                $addon->name = $addonData->title;
+                $addon->price = $addonData->discounted_price;
+            }
         }
         return $this->sendResponse([
             'orders' => $orders,
