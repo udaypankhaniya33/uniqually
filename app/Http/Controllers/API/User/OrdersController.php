dfe85a1c0d8e6b5b9f9d987c31360ff40f44c323
Buddhika Herath <buddhikaherath.dev@gmail.com>
2020-04-21 23:42:45 +0530

    Enable or Disable paypal payments at the order submission

--- app/Http/Controllers/API/User/OrdersController.php
+++ app/Http/Controllers/API/User/OrdersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API\User;
 
+use App\AppSetting;
 use App\Http\Controllers\API\BaseController;
 use App\Jobs\SendOnboardingEmail;
 use App\Jobs\SendOrderConfirmation;
@@ -69,112 +70,31 @@ class OrdersController extends BaseController
     public function store() {
         $currentUser = Auth::user();
         //$location = Location::get(request()->ip());
-        $orderSummery = [];
-        $orderValue = 0;
-        $quantity = 1;
-        $itemEach = 0;
-        if(request()->has('base_package_id', 'paypal_order_id', 'paypal_payer_id', 'paypal_auth_token')) {
-            if($this->authorizePayment(request('paypal_order_id'), null)){
-                $payPalOrderAuthorizedId = $this->payPalSuccess->purchase_units[0]->payments->authorizations[0]->id;
-                if($this->capturePayment($payPalOrderAuthorizedId)){
-                    $selectedPackage = Package::with('packageCategory')->find(request('base_package_id'));
-                    $itemEach = $selectedPackage->discounted_price;
-                    if (request()->has(['is_annual', 'expense']) && $selectedPackage->packageCategory->title === 'Bookkeeping') {
-                        $discount = (float)$selectedPackage->discounted_price * 20 / 100;
-                        $selectedPackage->discounted_price = request('is_annual') === 'true' ?
-                            (float)$selectedPackage->discounted_price - $discount : $selectedPackage->discounted_price;
-                        $selectedPackage->discounted_price = round($selectedPackage->discounted_price);
-                        switch (request('expense')) {
-                            case 20000:
-                                $selectedPackage->discounted_price = request('is_annual') === 'true' ?
-                                    (float)$selectedPackage->discounted_price + 40 :
-                                    (float)$selectedPackage->discounted_price + 50;
-                                break;
-                            case 30000:
-                                $selectedPackage->discounted_price = request('is_annual') === 'true' ?
-                                    (float)$selectedPackage->discounted_price + (40 * 2) :
-                                    (float)$selectedPackage->discounted_price + (50 * 2);
-                                break;
-                            case 40000:
-                                $selectedPackage->discounted_price = request('is_annual') === 'true' ?
-                                    (float)$selectedPackage->discounted_price + (40 * 3) :
-                                    (float)$selectedPackage->discounted_price + (50 * 3);
-                                break;
-                            case 50000:
-                                $selectedPackage->discounted_price = request('is_annual') === 'true' ?
-                                    (float)$selectedPackage->discounted_price + (40 * 4) :
-                                    (float)$selectedPackage->discounted_price + (50 * 4);
-                        }
-                        $itemEach = (float)$selectedPackage->discounted_price;
-                        if (request('is_annual') === 'true') {
-                            $selectedPackage->discounted_price = (float)$selectedPackage->discounted_price * 12;
-                            $currentYear = Carbon::now()->year;
-                            $selectedPackage->title = $selectedPackage->title . ' ( ' . $currentYear . ' )';
-                            $quantity = 12;
-                        }
+        $appSettings = AppSetting::where('key', 'is_paypal_after_submission')->first();
+        $isPayPalEnabled = $appSettings->value == true ? false : true;
+        $isAnnual = request()->has('is_annual') ? request('is_annual') : false;
+        $expense = request()->has('expense') ? request('expense') : null;
+        $packageAddons = request()->has('package_addons') ? request('package_addons') : null;
+
+        if($isPayPalEnabled){
+            if(request()->has('base_package_id', 'paypal_order_id', 'paypal_payer_id', 'paypal_auth_token')){
+                if($this->authorizePayment(request('paypal_order_id'), null)){
+                    $payPalOrderAuthorizedId = $this->payPalSuccess->purchase_units[0]->payments->authorizations[0]->id;
+                    if($this->capturePayment($payPalOrderAuthorizedId)){
+                        return $this->order(
+                            request('base_package_id'),
+                            $isAnnual,
+                            $expense,
+                            $currentUser,
+                            $packageAddons,
+                            $isPayPalEnabled);
+                    }else{
+                        return $this->sendError($this->payPalError->details[0]->issue, [
+                            'error' => [
+                                'desc' => $this->payPalError->details[0]->description
+                            ]
+                        ], 422);
                     }
-                    $orderValue = $orderValue + (float)$selectedPackage->discounted_price;
-                    $createdOrder = new Order([
-                        'custom_ind' => strtoupper(uniqid()),
-                        'order_created_by' => $currentUser->id,
-                        'order_creator_location' => null,
-                        'net_value' => $orderValue,
-                        'base_package_id' => $selectedPackage->id
-                    ]);
-                    $createdOrder->save();
-                    array_push($orderSummery, [
-                        'item' => $selectedPackage->title,
-                        'quantity' => $quantity,
-                        'costPerItem' => (float)$itemEach,
-                        'totalCost' => number_format((float)$selectedPackage->discounted_price, 2, '.', '')
-                    ]);
-                    if (request()->has('package_addons') && request('package_addons') !== null) {
-                        $selectedPackageAddonsWithQty = explode(',', request('package_addons'));
-                        foreach ($selectedPackageAddonsWithQty as $index => $addon) {
-                            $addonAndQuantity = explode(':', $addon);
-                            $selectedAddonId = $addonAndQuantity[0];
-                            $selectedAddonQuantity = $addonAndQuantity[1];
-                            $selectedAddon = PackageAddon::find($selectedAddonId);
-                            if ($selectedAddon !== null) {
-                                $cost = (float)$selectedAddon->discounted_price * $selectedAddonQuantity;
-                                $orderValue = $orderValue + $cost;
-                                $orderAddonCreated = new OrderAddon([
-                                    'order_id' => $createdOrder->id,
-                                    'package_addon_id' => $selectedAddon->id,
-                                    'quantity' => $selectedAddonQuantity
-                                ]);
-                                $orderAddonCreated->save();
-                                array_push($orderSummery, [
-                                    'item' => $selectedAddon->title,
-                                    'quantity' => (int)$selectedAddonQuantity,
-                                    'costPerItem' => (float)$selectedAddon->discounted_price,
-                                    'totalCost' => number_format((float)$cost, 2, '.', '')
-                                ]);
-                            }
-                        }
-                    }
-                    Order::where('id', $createdOrder->id)->update([
-                        'net_value' => $orderValue
-                    ]);
-                    $payPalSuccessRes = new PayPalSuccessResponse([
-                        'response'  => json_encode($this->payPalSuccess),
-                        'order_id' =>  $createdOrder->id
-                    ]);
-                    $payPalSuccessRes->save();
-                    dispatch(new SendOrderConfirmation(
-                        $createdOrder->custom_ind,
-                        Carbon::now(),
-                        $orderSummery,
-                        $currentUser->email,
-                        $orderValue,
-                        $currentUser->name
-                    ));
-                    dispatch(new SendOnboardingEmail($currentUser,  $selectedPackage->packageCategory->title));
-                    return $this->sendResponse([
-                        'summery' => $orderSummery,
-                        'total_cost' => number_format((float)$orderValue, 2, '.', ''),
-                        'order_id' => $createdOrder->custom_ind
-                    ], 'Order has been submitted successfully!');
                 }else{
                     return $this->sendError($this->payPalError->details[0]->issue, [
                         'error' => [
@@ -183,23 +103,142 @@ class OrdersController extends BaseController
                     ], 422);
                 }
             }else{
-                return $this->sendError($this->payPalError->details[0]->issue, [
+                return $this->sendError('Please provide valid data', [
                     'error' => [
-                        'desc' => $this->payPalError->details[0]->description
+                        'base_package_id' => 'Base package is required to initiate order',
+                        'paypal_payer_id' => 'PayPal payer id is required',
+                        'paypal_auth_token' => 'PayPal authorization token is required',
+                        'paypal_order_id' => 'PayPal order id is required'
                     ]
                 ], 422);
             }
-        }else {
-            return $this->sendError('Please provide valid data', [
-                'error' => [
-                    'base_package_id' => 'Base package is required to initiate order',
-                    'paypal_payer_id' => 'PayPal payer id is required',
-                    'paypal_auth_token' => 'PayPal authorization token is required',
-                    'paypal_order_id' => 'PayPal order id is required'
-                ]
-            ], 422);
+        }else{
+            if(request()->has('base_package_id')){
+                return $this->order(
+                    request('base_package_id'),
+                    $isAnnual,
+                    $expense,
+                    $currentUser,
+                    $packageAddons,
+                    $isPayPalEnabled);
+            }else{
+                return $this->sendError('Please provide valid data', [
+                    'error' => [
+                        'base_package_id' => 'Base package is required to initiate order'
+                    ]
+                ], 422);
+            }
         }
+    }
 
+    private function order($basePackageId, $isAnnual = false, $expense = null, $currentUser, $packageAddons = null,
+                           $isPaypalEnabled = false) {
+        $discount = 0;
+        $orderValue = 0;
+        $orderSummery = [];
+        $orderValue = 0;
+        $quantity = 1;
+        $itemEach = 0;
+        $selectedPackage = Package::with('packageCategory')->find($basePackageId);
+        $itemEach = $selectedPackage->discounted_price;
+        if ($isAnnual && $expense !== null && $selectedPackage->packageCategory->title === 'Bookkeeping') {
+            $discount = (float)$selectedPackage->discounted_price * 20 / 100;
+            $selectedPackage->discounted_price =$isAnnual === true ?
+                (float)$selectedPackage->discounted_price - $discount : $selectedPackage->discounted_price;
+            $selectedPackage->discounted_price = round($selectedPackage->discounted_price);
+            switch ($expense) {
+                case 20000:
+                    $selectedPackage->discounted_price = $isAnnual === true ?
+                        (float)$selectedPackage->discounted_price + 40 :
+                        (float)$selectedPackage->discounted_price + 50;
+                    break;
+                case 30000:
+                    $selectedPackage->discounted_price = $isAnnual === true ?
+                        (float)$selectedPackage->discounted_price + (40 * 2) :
+                        (float)$selectedPackage->discounted_price + (50 * 2);
+                    break;
+                case 40000:
+                    $selectedPackage->discounted_price = $isAnnual === true ?
+                        (float)$selectedPackage->discounted_price + (40 * 3) :
+                        (float)$selectedPackage->discounted_price + (50 * 3);
+                    break;
+                case 50000:
+                    $selectedPackage->discounted_price = $isAnnual === true ?
+                        (float)$selectedPackage->discounted_price + (40 * 4) :
+                        (float)$selectedPackage->discounted_price + (50 * 4);
+            }
+            $itemEach = (float)$selectedPackage->discounted_price;
+            if ($isAnnual === true) {
+                $selectedPackage->discounted_price = (float)$selectedPackage->discounted_price * 12;
+                $currentYear = Carbon::now()->year;
+                $selectedPackage->title = $selectedPackage->title . ' ( ' . $currentYear . ' )';
+                $quantity = 12;
+            }
+        }
+        $orderValue = $orderValue + (float)$selectedPackage->discounted_price;
+        $createdOrder = new Order([
+            'custom_ind' => strtoupper(uniqid()),
+            'order_created_by' => $currentUser->id,
+            'order_creator_location' => null,
+            'net_value' => $orderValue,
+            'base_package_id' => $selectedPackage->id
+        ]);
+        $createdOrder->save();
+        array_push($orderSummery, [
+            'item' => $selectedPackage->title,
+            'quantity' => $quantity,
+            'costPerItem' => (float)$itemEach,
+            'totalCost' => number_format((float)$selectedPackage->discounted_price, 2, '.', '')
+        ]);
+        if ($packageAddons !== null) {
+            $selectedPackageAddonsWithQty = explode(',', $packageAddons);
+            foreach ($selectedPackageAddonsWithQty as $index => $addon) {
+                $addonAndQuantity = explode(':', $addon);
+                $selectedAddonId = $addonAndQuantity[0];
+                $selectedAddonQuantity = $addonAndQuantity[1];
+                $selectedAddon = PackageAddon::find($selectedAddonId);
+                if ($selectedAddon !== null) {
+                    $cost = (float)$selectedAddon->discounted_price * $selectedAddonQuantity;
+                    $orderValue = $orderValue + $cost;
+                    $orderAddonCreated = new OrderAddon([
+                        'order_id' => $createdOrder->id,
+                        'package_addon_id' => $selectedAddon->id,
+                        'quantity' => $selectedAddonQuantity
+                    ]);
+                    $orderAddonCreated->save();
+                    array_push($orderSummery, [
+                        'item' => $selectedAddon->title,
+                        'quantity' => (int)$selectedAddonQuantity,
+                        'costPerItem' => (float)$selectedAddon->discounted_price,
+                        'totalCost' => number_format((float)$cost, 2, '.', '')
+                    ]);
+                }
+            }
+        }
+        Order::where('id', $createdOrder->id)->update([
+            'net_value' => $orderValue
+        ]);
+        dispatch(new SendOrderConfirmation(
+            $createdOrder->custom_ind,
+            Carbon::now(),
+            $orderSummery,
+            $currentUser->email,
+            $orderValue,
+            $currentUser->name
+        ));
+        dispatch(new SendOnboardingEmail($currentUser,  $selectedPackage->packageCategory->title));
+        if($isPaypalEnabled){
+            $payPalSuccessRes = new PayPalSuccessResponse([
+                'response'  => json_encode($this->payPalSuccess),
+                'order_id' =>  $createdOrder->id
+            ]);
+            $payPalSuccessRes->save();
+        }
+        return $this->sendResponse([
+            'summery' => $orderSummery,
+            'total_cost' => number_format((float)$orderValue, 2, '.', ''),
+            'order_id' => $createdOrder->custom_ind
+        ], 'Order has been submitted successfully!');
     }
 
     private function authorizePayment($payPalOrderId, $errorCondition) {
