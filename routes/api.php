bb856a084f40bee62d71aeab791cf1ba73def821
Manthika <tharumudu@gmail.com>
2020-07-09 11:54:17 +0530

    Created API endpoint to retrieve all formation steps by entity id and location id

--- routes/api.php
+++ routes/api.php
@@ -100,4 +100,5 @@ Route::group(['prefix' => 'entity', 'middleware' => 'cors'], function()
 {
     Route::get('entity-types', 'API\Guest\Entity\EntityTypesController@index');
     Route::get('locations', 'API\Guest\Entity\LocationsController@index');
+    Route::get('formation-steps/{entityId}/{locationId}', 'API\Guest\Entity\FormationStepsController@getFormationStepsByEntityAndLocation');
 });
